46d855a94d5c51427c0734ada36cbb089556aa33
SXXXXXX <16203122@stu.nchu.edu.cn>
2019-06-17 08:17:47 +0800

    创建了项目

---
+++ hw1/src/main/java/webadv/s162031/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package webadv.s162031.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+
+}
